059344e59fdb593b6e6bca1dd97a8a6a9abafa6f
iwshan <iwshan@sina.com>
2013-02-18 11:16:08 +0800

    add WuTextVC

---
+++ text_43/WuTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  WuTestViewController.h
+//  text_43
+//
+//  Created by wusan on 13-2-18.
+//  Copyright (c) 2013年 alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WuTestViewController : UIViewController
+
+@end
---
+++ text_43/WuTestViewController.m
@@ -0,0 +1,38 @@
+//
+//  WuTestViewController.m
+//  text_43
+//
+//  Created by wusan on 13-2-18.
+//  Copyright (c) 2013年 alex. All rights reserved.
+//
+
+#import "WuTestViewController.h"
+
+@interface WuTestViewController ()
+
+@end
+
+@implementation WuTestViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
