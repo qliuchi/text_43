3e51c863a9c0860c6eab930c6d7e23dd41d019b2
qliuchi <546643744@qq.com>
2013-02-18 10:44:36 +0800

    Initial Commit

---
+++ text_43/TestAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TestAppDelegate.h
+//  text_43
+//
+//  Created by Ibokan on 13-2-18.
+//  Copyright (c) 2013年 alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ text_43/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  text_43
+//
+//  Created by Ibokan on 13-2-18.
+//  Copyright (c) 2013年 alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TestAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TestAppDelegate class]));
+    }
+}
