656e4e663687226cf74c6fe10a311f441b77b336
xsyu <yrxxsyu@163.com>
2013-02-18 11:23:18 +0800

    add yu

---
+++ YuViewController.h
@@ -0,0 +1,13 @@
+//
+//  YuViewController.h
+//  text_43
+//
+//  Created by Ibokan on 13-2-18.
+//  Copyright (c) 2013年 alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YuViewController : UIViewController
+
+@end
---
+++ YuViewController.m
@@ -0,0 +1,38 @@
+//
+//  YuViewController.m
+//  text_43
+//
+//  Created by Ibokan on 13-2-18.
+//  Copyright (c) 2013年 alex. All rights reserved.
+//
+
+#import "YuViewController.h"
+
+@interface YuViewController ()
+
+@end
+
+@implementation YuViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
