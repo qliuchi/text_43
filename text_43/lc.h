83393fdc82e903db627cdaa222a46b20c75c2cfc
qliuchi <546643744@qq.com>
2013-02-18 11:10:32 +0800

    add a.h
    add a.h

---
+++ text_43/lc.h
@@ -0,0 +1,14 @@
+//
+//  lc.h
+//  text_43
+//
+//  Created by Ibokan on 13-2-18.
+//  Copyright (c) 2013年 alex. All rights reserved.
+//
+
+#ifndef text_43_lc_h
+#define text_43_lc_h
+
+
+int a 
+#endif
